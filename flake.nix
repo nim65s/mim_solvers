7df5186d5a91e19ff2010fb3619b55db8e1c4426
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-10-06 22:13:08 +0200

    nix: BUILD_STANDALONE_PYTHON_INTERFACE

--- flake.nix
+++ flake.nix
@@ -11,22 +11,23 @@
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
       systems = inputs.nixpkgs.lib.systems.flakeExposed;
       perSystem =
-        { pkgs, self', ... }:
+        {
+          lib,
+          pkgs,
+          self',
+          ...
+        }:
         {
           apps.default = {
             type = "app";
             program = pkgs.python3.withPackages (_: [ self'.packages.default ]);
           };
-          devShells.default = pkgs.mkShell {
-            inputsFrom = [ self'.packages.default ];
-            packages = [ (pkgs.python3.withPackages (p: [p.tomlkit])) ];
-          };
           packages = {
-            default = self'.packages.mim-solvers;
-            mim-solvers = pkgs.python3Packages.mim-solvers.overrideAttrs (old: {
-              src = pkgs.lib.fileset.toSource {
+            default = self'.packages.py-mim-solvers;
+            mim-solvers = pkgs.mim-solvers.overrideAttrs {
+              src = lib.fileset.toSource {
                 root = ./.;
-                fileset = pkgs.lib.fileset.unions [
+                fileset = lib.fileset.unions [
                   ./benchmarks
                   ./bindings
                   ./examples
@@ -38,7 +39,31 @@
                   ./package.xml
                 ];
               };
-            });
+            };
+            py-mim-solvers = pkgs.python3Packages.toPythonModule (
+              self'.packages.mim-solvers.overrideAttrs (super: {
+                pname = "py-${super.pname}";
+                postPatch = "";
+                cmakeFlags = super.cmakeFlags ++ [
+                  (lib.cmakeBool "BUILD_PYTHON_INTERFACE" true)
+                  (lib.cmakeBool "BUILD_STANDALONE_PYTHON_INTERFACE" true)
+                ];
+                nativeBuildInputs = super.nativeBuildInputs ++ [
+                  pkgs.python3Packages.python
+                ];
+                propagatedBuildInputs = [
+                  pkgs.python3Packages.crocoddyl
+                  pkgs.python3Packages.osqp
+                  pkgs.python3Packages.proxsuite
+                  pkgs.python3Packages.scipy
+                  self'.packages.mim-solvers
+                ]
+                ++ super.propagatedBuildInputs;
+                nativeCheckInputs = [
+                  pkgs.python3Packages.pythonImportsCheckHook
+                ];
+              })
+            );
           };
         };
     };
